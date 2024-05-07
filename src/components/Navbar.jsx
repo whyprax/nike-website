2b7a7534aad2d3915bdc6e1d2982b33d410751ca
prathamdubey <prathamd67@gmail.com>
2024-05-08 02:52:56 +0530

    Navbar Part 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,29 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants/index";
+
 const Navbar = () => {
-  return <>Navbar</>;
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} width={130} height={29} alt="" />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((navLink) => (
+            <li key={navLink.label}>
+              <a
+                href={navLink.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {navLink.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
